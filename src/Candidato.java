bffaf5b5b6165a80750fcfe71e3f254c643d8734
lucape <lucaspedrogit@gmail.com>
2023-03-14 08:40:32 -0300

    Conclusao de criacao de Objeto

--- src/Candidato.java
+++ src/Candidato.java
@@ -2,13 +2,12 @@ import java.time.LocalDate;
 
 public class Candidato {
     public String nome;
-    public LocalDate dataNasc;
+    public LocalDate dataNascimento;
     public String email;
     public String celular;
     public String escolaridade;
     public String funcao;
     public double pretencaoSalarial;
-    public boolean empregado;
-    public boolean estagio;
-
+    public boolean estaEmpregado;
+    public boolean aceitaEstagio;
 }
--- src/CandidatoTeste.java
+++ src/CandidatoTeste.java
@@ -1,5 +1,16 @@
 public class CandidatoTeste {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        //Instanciar um objeto
+        Candidato candidato = new Candidato();
+        candidato.nome = "Lucape";
+        candidato.dataNascimento = LocalDate.of(year: 2000, mounth: 10, dayOfMounth: 1);
+        candidato.email = "lucaspedrogit@gmail.com";
+        candidato.celular= "(11) 92256-7475";
+        candidato.escolaridade = "tecnico";
+        candidato.funcao = "aluno";
+        candidato.pretencaoSalarial = 15000;
+        candidato.estaEmpregado = false;
+        candidato.aceitaEstagio = true;
     }
 }
