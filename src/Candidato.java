18885d324b1fb4c6b65644ea5c8b46e65d9afa11
lucape <lucaspedrogit@gmail.com>
2023-03-14 08:20:06 -0300

    Inclusao de lasse Candidato

---
+++ src/Candidato.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+
+public class Candidato {
+    public String nome;
+    public LocalDate data_nasc;
+    public String email;
+    public int celular;
+    public boolean escolaridade;
+    public boolean funcao;
+    public float pretencao_salarial;
+    public boolean empregado;
+    public boolean estagio;
+
+}
---
+++ src/CandidatoTeste.java
@@ -0,0 +1,5 @@
+public class CandidatoTeste {
+    public static void main(String[] args) {
+
+    }
+}
